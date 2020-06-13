817c06de5119330fe9702ae7b2b66e265ee95278
Atharva-2157 <atharvadiwan65@gmail.com>
2020-06-13 18:01:42 +0530

    second commit

--- spaceGame.py
+++ spaceGame.py
@@ -2,6 +2,7 @@ import random
 import pygame
 
 pygame.init()
+pygame.mixer.init()
 
 SCREENWIDTH = 900
 SCREENHEIGHT = 715
@@ -20,14 +21,55 @@ GREEN = (0, 255, 0)
 FPS = 110
 FPS_CLOCK = pygame.time.Clock()
 
+
+WELCOME_IMG = pygame.image.load('Images\\welcomeSpace.jpeg')
 BG_IMG = pygame.image.load('Images\\space.jpeg')
 MAIN_PLANE = pygame.image.load('Images\\player_ship.png')
 PLANE_BULLET = pygame.image.load('Images\\bullet.png')
 ENEMY_PLANE = pygame.image.load('Images\\enemy_ship.png')
+DIGITS = {
+    '0': pygame.image.load('Images\\0.png'),
+    '1': pygame.image.load('Images\\1.png'),
+    '2': pygame.image.load('Images\\2.png'),
+    '3': pygame.image.load('Images\\3.png'),
+    '4': pygame.image.load('Images\\4.png'),
+    '5': pygame.image.load('Images\\5.png'),
+    '6': pygame.image.load('Images\\6.png'),
+    '7': pygame.image.load('Images\\7.png'),
+    '8': pygame.image.load('Images\\8.png'),
+    '9': pygame.image.load('Images\\9.png'),
+}
+
+LABELS = {
+    'PLAY': pygame.image.load('Images\\play.png'),
+    'QUIT': pygame.image.load('Images\\quit.png'),
+}
 
 BG_IMG = pygame.transform.scale(BG_IMG, (SCREENWIDTH, SCREENHEIGHT)).convert_alpha()
+WELCOME_IMG = pygame.transform.scale(WELCOME_IMG, (SCREENWIDTH, SCREENHEIGHT)).convert_alpha()
 ENEMY_PLANE = pygame.transform.rotate(ENEMY_PLANE, 180)
 ENEMY_PLANE = pygame.transform.scale(ENEMY_PLANE, (50, 40)).convert_alpha()
+
+
+class Button:
+    def __init__(self, text, color, x, y, width, height):
+        self.text = text
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self):
+        pygame.draw.rect(WINDOW, self.color, [self.x, self.y, self.width, self.height])
+        WINDOW.blit(self.text, (self.x, self.y))
+
+    def isHover(self, pos):
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
+                return True
+
+        return False
 
 
 class Bullet:
@@ -59,7 +101,7 @@ class Plane:
 
     def moveBullet(self):
         for bullet in self.bullets:
-            bullet[2] -= 1
+            bullet[2] -= 2
 
     def deleteExtraBullet(self):
         for bullet in self.bullets:
@@ -82,6 +124,12 @@ class Plane:
             return True
         else:
             return False
+
+    def healthBar(self):
+        k = (self.plane_w + 1) // 10
+        pygame.draw.rect(WINDOW, RED, [self.plane_x, self.plane_y + self.plane_h + 10, self.plane_w, 7])
+        pygame.draw.rect(WINDOW, GREEN, [self.plane_x + (k * (10 - self.life)), self.plane_y + self.plane_h + 10,
+                                         self.plane_w - (k * (10 - self.life)), 7])
 
     def showBullets(self):
         for bullet in self.bullets:
@@ -107,7 +155,7 @@ class Enemy:
         self.enemy_y += 1
 
     def newBullet(self):
-        if self.bullets[-1][1] - self.enemy_y > random.randint(300, 400):
+        if self.bullets[-1][1] - self.enemy_y > random.randint(220, 250):
             self.bullets.append([self.enemy_x + (self.enemy_w // 2) - 3,
                                  self.enemy_y + self.enemy_h - 17])
 
@@ -151,6 +199,46 @@ class Enemy:
                     self.bullets.pop(0)
 
 
+def welcomeWindow():
+    global EXIT_GAME
+    play_button = Button(LABELS['PLAY'], GREEN, SCREENWIDTH // 2 - 10, 250, 65, 35)
+    quit_button = Button(LABELS['QUIT'], GREEN, SCREENWIDTH // 2 - 10, 300, 65, 35)
+
+    while not EXIT_GAME:
+        for event in pygame.event.get():
+            position = pygame.mouse.get_pos()
+            if event.type == pygame.QUIT:
+                EXIT_GAME = True
+
+            if event.type == pygame.MOUSEMOTION:
+                if play_button.isHover(position):
+                    play_button.color = YELLOW
+                else:
+                    play_button.color = GREEN
+
+                if quit_button.isHover(position):
+                    quit_button.color = RED
+                else:
+                    quit_button.color = GREEN
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.isHover(position):
+                    return
+                if quit_button.isHover(position):
+                    EXIT_GAME = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return
+
+        # WINDOW.fill(WHITE)
+        WINDOW.blit(WELCOME_IMG, (0, 0))
+        play_button.draw()
+        quit_button.draw()
+        pygame.display.update()
+        FPS_CLOCK.tick(FPS)
+
+
 def gameLoop():
     global EXIT_GAME
     plane = Plane()
@@ -168,6 +256,7 @@ def gameLoop():
                         return
 
             WINDOW.fill(WHITE)
+
         else:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -175,10 +264,10 @@ def gameLoop():
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
-                        plane.change_x = -2
+                        plane.change_x = -5
 
                     if event.key == pygame.K_RIGHT:
-                        plane.change_x = 2
+                        plane.change_x = 5
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
@@ -198,6 +287,12 @@ def gameLoop():
             # WINDOW.blit(BG_IMG, (0, 0))
             WINDOW.fill(WHITE)
 
+            score_list = [x for x in str(score)]
+            k = 0
+            for i in score_list:
+                k += 1
+                WINDOW.blit(DIGITS[i], (k * 25, 10))
+
             plane.newBullet()
             plane.moveBullet()
             plane.drawBullet()
@@ -208,7 +303,7 @@ def gameLoop():
                 opponent = Enemy()
                 enemy_list.append(opponent)
 
-            if enemy_list[-1].enemy_y >= random.randint(270, 370):
+            if enemy_list[-1].enemy_y >= random.randint(50, 100):
                 opponent = Enemy()
                 enemy_list.append(opponent)
 
@@ -237,9 +332,13 @@ def gameLoop():
 
                 i += 1
 
+                plane.healthBar()
+
         pygame.display.update()
+        FPS_CLOCK.tick(FPS)
 
 
 if __name__ == '__main__':
     while not EXIT_GAME:
+        welcomeWindow()
         gameLoop()
